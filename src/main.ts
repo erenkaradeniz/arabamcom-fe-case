2075acf6354ddcd7edb658351743e62ce2b88e24
Eren Karadeniz <erenkaradeniz@protonmail.com>
2026-01-17 23:29:30 +0300

    chore: initialize tailwind css v4.1

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { VueQueryPlugin } from '@tanstack/vue-query'
 
+import './assets/main.css'
+
 import App from './App.vue'
 import router from './router'
 
